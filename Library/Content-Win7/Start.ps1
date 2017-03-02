7dbb8e45e06ccdf00f66dddc10238e4204f7e604
June Rhodes <jrhodes@redpointgames.com.au>
2017-03-02 14:36:23 +1100

    Start-Transcript and have dedicated output directory

--- Library/Content-Win7/Start.ps1
+++ Library/Content-Win7/Start.ps1
@@ -2,8 +2,15 @@ $ErrorActionPreference = "Stop"
 trap
 {
     Write-Error $_
+    Stop-Transcript
     exit 1
 }
+
+if (!(Test-Path "C:\Output-Win7")) {
+    mkdir "C:\Output-Win7"
+}
+Start-Transcript -Path "C:\Output-Win7\Transcript.log"
+
 Import-Module "C:\Program Files (x86)\AutoIt3\AutoItX\AutoItX.psd1"
 Write-Output "AutoItX Imported"
 
@@ -33,7 +40,7 @@ function Take-Screenshot($Path) {
     $bmp.Dispose()
 }
 
-Take-Screenshot -Path "C:\Screenshot01.png"
+Take-Screenshot -Path "C:\Output-Win7\Screenshot01.png"
 
 Write-Output "Launching Steam..."
 Invoke-AU3Run -Program "C:\Program Files (x86)\Steam\Steam.exe"
@@ -41,30 +48,30 @@ Invoke-AU3Run -Program "C:\Program Files (x86)\Steam\Steam.exe"
 # Handle the "Create Account or Login" screen
 Wait-AU3Win -Title "Steam"
 Move-AU3Win -X 0 -Y 0 -Title "Steam"
-Take-Screenshot -Path "C:\Screenshot02.png"
+Take-Screenshot -Path "C:\Output-Win7\Screenshot02.png"
 Start-Sleep -Seconds 2
 Invoke-AU3MouseClick -X 211 -Y 335
 
 # Handle the login screen
 Wait-AU3Win -Title "Steam Login"
 Move-AU3Win -X 0 -Y 0 -Title "Steam Login"
-Take-Screenshot -Path "C:\Screenshot03.png"
+Take-Screenshot -Path "C:\Output-Win7\Screenshot03.png"
 Start-Sleep -Seconds 1
 Invoke-AU3MouseClick -X 130 -Y 95
 Start-Sleep -Seconds 1
 Send-AU3Key -Key $SteamUsername
 Start-Sleep -Seconds 1
-Take-Screenshot -Path "C:\Screenshot04.png"
+Take-Screenshot -Path "C:\Output-Win7\Screenshot04.png"
 Invoke-AU3MouseClick -X 130 -Y 134
 Start-Sleep -Seconds 1
 Send-AU3Key -Key $SteamPassword
 Start-Sleep -Seconds 1
-Take-Screenshot -Path "C:\Screenshot05.png"
+Take-Screenshot -Path "C:\Output-Win7\Screenshot05.png"
 Invoke-AU3MouseClick -X 123 -Y 162 # Tick Remember Me
 Start-Sleep -Seconds 1
-Take-Screenshot -Path "C:\Screenshot06.png"
+Take-Screenshot -Path "C:\Output-Win7\Screenshot06.png"
 Invoke-AU3MouseClick -X 144 -Y 193 # and click Login!
 
-Take-Screenshot -Path "C:\Screenshot07.png"
+Take-Screenshot -Path "C:\Output-Win7\Screenshot07.png"
 
 exit 0
