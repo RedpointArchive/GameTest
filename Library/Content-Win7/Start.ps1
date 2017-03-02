56acaba663fb85dc63b18b5014b7852e109c116c
June Rhodes <jrhodes@redpointgames.com.au>
2017-03-02 14:18:56 +1100

    $PSScriptRoot is not available on PowerShell 2 or lower

--- Library/Content-Win7/Start.ps1
+++ Library/Content-Win7/Start.ps1
@@ -7,7 +7,8 @@ trap
 Import-Module "C:\Program Files (x86)\AutoIt3\AutoItX\AutoItX.psd1"
 Write-Output "AutoItX Imported"
 
-Set-Location $PSScriptRoot
+$_PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+Set-Location $_PSScriptRoot
 
 if (Test-Path Credentials.ps1) {
     . .\Credentials.ps1
@@ -19,20 +20,20 @@ if ([string].IsNullOrWhiteSpace($SteamUsername) -or [string].IsNullOrWhiteSpace(
 }
 
 [Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-function Take-Screenshot($path) {
+function Take-Screenshot($Path) {
     $bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1024, 768)
     $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
     $graphics = [Drawing.Graphics]::FromImage($bmp)
 
     $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
 
-    $bmp.Save($path)
+    $bmp.Save($Path)
 
     $graphics.Dispose()
     $bmp.Dispose()
 }
 
-Take-Screenshot "C:\Screenshot01.png"
+Take-Screenshot -Path "C:\Screenshot01.png"
 
 Write-Output "Launching Steam..."
 Invoke-AU3Run -Program "C:\Program Files (x86)\Steam\Steam.exe"
@@ -40,30 +41,30 @@ Invoke-AU3Run -Program "C:\Program Files (x86)\Steam\Steam.exe"
 # Handle the "Create Account or Login" screen
 Wait-AU3Win -Title "Steam"
 Move-AU3Win -X 0 -Y 0 -Title "Steam"
-Take-Screenshot "C:\Screenshot02.png"
+Take-Screenshot -Path "C:\Screenshot02.png"
 Start-Sleep -Seconds 2
 Invoke-AU3MouseClick -X 211 -Y 335
 
 # Handle the login screen
 Wait-AU3Win -Title "Steam Login"
 Move-AU3Win -X 0 -Y 0 -Title "Steam Login"
-Take-Screenshot "C:\Screenshot03.png"
+Take-Screenshot -Path "C:\Screenshot03.png"
 Start-Sleep -Seconds 1
 Invoke-AU3MouseClick -X 130 -Y 95
 Start-Sleep -Seconds 1
 Send-AU3Key -Key $SteamUsername
 Start-Sleep -Seconds 1
-Take-Screenshot "C:\Screenshot04.png"
+Take-Screenshot -Path "C:\Screenshot04.png"
 Invoke-AU3MouseClick -X 130 -Y 134
 Start-Sleep -Seconds 1
 Send-AU3Key -Key $SteamPassword
 Start-Sleep -Seconds 1
-Take-Screenshot "C:\Screenshot05.png"
+Take-Screenshot -Path "C:\Screenshot05.png"
 Invoke-AU3MouseClick -X 123 -Y 162 # Tick Remember Me
 Start-Sleep -Seconds 1
-Take-Screenshot "C:\Screenshot06.png"
+Take-Screenshot -Path "C:\Screenshot06.png"
 Invoke-AU3MouseClick -X 144 -Y 193 # and click Login!
 
-Take-Screenshot "C:\Screenshot07.png"
+Take-Screenshot -Path "C:\Screenshot07.png"
 
 exit 0
